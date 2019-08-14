9fb045ff620f336a75c875494ba06f4b73387d19
TsuyoshiOsawa <t2-osawa@ibetmac.local>
2019-08-14 19:28:48 +0900

    patch for me

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const { fork } = require('child_process');
 const { join } = require('path');
-const { libraryList } = require('setting');
+const { libraryList } = require('./setting');
 const { getClassesMapping, readDir, chunk } = require('./src/utils');
 
 const cpus = require('os').cpus().length;
 
 const arg = process.argv.slice(2)[0];
 const mode = arg && ((arg === 'reverse') || (arg === '-r')) ? 'reverse' : 'forward';
-let SEARCH_DIR = 'node_modules';
 const classesMapping = getClassesMapping();
-for (let target of libraryList) {
-  SEARCH_DIR = SEARCH_DIR + '/' + target;
+console.log(libraryList)
+for (let index in libraryList) {
+  let SEARCH_DIR = 'node_modules';
+  SEARCH_DIR = SEARCH_DIR + '/' + libraryList[index];
   const files = readDir(SEARCH_DIR);
   console.log(`Jetifier found ${files.length} file(s) to ${mode}-jetify. Using ${cpus} workers...`);
 
--- setting.js
+++ setting.js
@@ -1,9 +1,14 @@
-libraryList = [
-              'react-native-gesture-handler',
-              'react-native-image-picker',
-              'react-native-push-notification',
-              'react-native-rsa-native',
-              'react-native-sensitive-info',
-              'react-native-tcp',
-              'react-native-udp'
-              ];
+const libraryList = [
+    'react-native-gesture-handler',
+    'react-native-image-picker',
+    'react-native-push-notification',
+    'react-native-rsa-native',
+    'react-native-sensitive-info',
+    'react-native-tcp',
+    'react-native-udp',
+    '@react-native-community/slider',
+    'react-native-camera'
+];
+module.exports = {
+    libraryList
+};
