2ddd3b6936256648f19848ae6efd1dc422db5a6e
TsuyoshiOsawa <t2-osawa@ibetmac.local>
2019-08-14 19:14:30 +0900

    patch for ibet

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const { fork } = require('child_process');
 const { join } = require('path');
+const { libraryList } = require('setting');
 const { getClassesMapping, readDir, chunk } = require('./src/utils');
 
 const cpus = require('os').cpus().length;
 
 const arg = process.argv.slice(2)[0];
 const mode = arg && ((arg === 'reverse') || (arg === '-r')) ? 'reverse' : 'forward';
-const SEARCH_DIR = 'node_modules';
-
+let SEARCH_DIR = 'node_modules';
 const classesMapping = getClassesMapping();
-const files = readDir(SEARCH_DIR);
+for (let target of libraryList) {
+  SEARCH_DIR = SEARCH_DIR + '/' + target;
+  const files = readDir(SEARCH_DIR);
+  console.log(`Jetifier found ${files.length} file(s) to ${mode}-jetify. Using ${cpus} workers...`);
 
-console.log(`Jetifier found ${files.length} file(s) to ${mode}-jetify. Using ${cpus} workers...`);
+  for (const filesChunk of chunk(files, cpus)) {
+    const worker = fork(join(__dirname, 'src', 'worker.js'));
+    worker.send({ filesChunk, classesMapping, mode });
+  }
 
-for (const filesChunk of chunk(files, cpus)) {
-  const worker = fork(join(__dirname, 'src', 'worker.js'));
-  worker.send({ filesChunk, classesMapping, mode });
 }
---
+++ setting.js
@@ -0,0 +1,9 @@
+libraryList = [
+              'react-native-gesture-handler',
+              'react-native-image-picker',
+              'react-native-push-notification',
+              'react-native-rsa-native',
+              'react-native-sensitive-info',
+              'react-native-tcp',
+              'react-native-udp'
+              ];
