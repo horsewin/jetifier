abb305d135800cdf42fe0631c653712bddd3036e
horsewin <mamamatz@gmail.com>
2019-11-05 12:46:04 +0900

    Update setting.js

--- setting.js
+++ setting.js
@@ -2,7 +2,6 @@ const libraryList = [
     'react-native-gesture-handler',
     'react-native-image-picker',
     'react-native-push-notification',
-    'react-native-rsa-native',
     'react-native-sensitive-info',
     'react-native-tcp',
     'react-native-udp',
